fd11a72150285db02727d937ab1a75e447f7b439
Charles Morin <charles.mor@outlook.fr>
2018-10-08 14:27:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 
-var app = module.exports = express();
+var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
